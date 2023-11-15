39bbfc0034d0cf69caca543b8ca15415e45e4aa9
StormLight14 <100660727+StormLight14@users.noreply.github.com>
2023-11-15 14:56:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Debian and Ubuntu script for CyberPatriot
 
 import os
+import subprocess
 
 # update apt repos and install package upgrades
 os.system("sudo apt update && sudo apt upgrade")
@@ -8,15 +9,18 @@ os.system("sudo apt update && sudo apt upgrade")
 # enable auto update
 print("todo")
 
-# install (if it for some reason isnt) and enable ufw
+# install ufw (if it for some reason isnt) and enable it
 os.system("sudo apt install ufw")
 os.system("sudo ufw enable")
 
 # disable root ssh login by replacing text in config
 os.system("sudo sed -i \"s/PermitRootLogin yes/PermitRootLogin no/g\" /etc/ssh/sshd_config")
 
+# update db for locate command
+os.system("sudo updatedb")
+
 # search for media files, prompt whether to delete or not
-print("todo")
+locate_output = subprocess.getoutput("locate *.mp3") + subprocess.getoutput("locate *.mp4")
 
 # disable anonymous ftp login
 print("todo")
