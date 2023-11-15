b011b2e9e4c5ee92694dbcc4fa5967e7655d9305
Storm <stormpower44@protonmail.com>
2023-11-14 20:14:42 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# Debian and Ubuntu shell script for CyberPatriot
+
+import os
+
+# update apt repos and install package upgrades
+os.system("sudo apt update && sudo apt upgrade")
+
+# install (if it for some reason isnt) and enable ufw
+os.system("sudo apt install ufw")
+os.system("sudo ufw enable")
+
+# disable root ssh login
+
+# search for media files, prompt whether to delete or not
+
+# disable anonymous ftp login
+
+# enable ssl for ftp
