9c7277a1900d99dbb34742bd2b2843af8fe87888
Storm <stormpower44@protonmail.com>
2023-11-14 20:49:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,23 @@ import os
 os.system("sudo apt update && sudo apt upgrade")
 
 # enable auto update
+print("todo")
 
 # install (if it for some reason isnt) and enable ufw
 os.system("sudo apt install ufw")
 os.system("sudo ufw enable")
 
 # disable root ssh login
+print("todo")
 
 # search for media files, prompt whether to delete or not
+print("todo")
 
 # disable anonymous ftp login
+print("todo")
 
 # enable ssl for ftp
+print("todo")
 
 # set mininum password age
+print("todo")
