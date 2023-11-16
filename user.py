7005bec76b07c69bff6cde972b980a76a695fba3
StormLight14 <100660727+StormLight14@users.noreply.github.com>
2023-11-16 08:32:34 -0700

    Update user.py

--- user.py
+++ user.py
@@ -1,4 +1,5 @@
 class User:
-  def __init__(username, is_admin):
+  def __init__(username, is_authorized, is_admin):
     self.username = username
+    self.is_authorized = is_authorized
     self.is_admin = is_admin
