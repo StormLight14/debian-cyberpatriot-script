19d73e1257aabe09e7bca36615c69cb5c61a16c0
Storm <stormpower44@protonmail.com>
2023-11-15 19:06:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -113,16 +113,32 @@ print("Set system password aging policies for future users.")
 users = []
 auth_users = []
 
-# set good password aging policies for current users
+
+# get all users on system and append to users list
 with open('/etc/passwd', 'r') as passwd_file:
     for line in passwd_file.readlines():
-        users.append(line.split(":")[0]) # only take user's name from /etc/passwd
+        if "/home/" in line: # only have actual users
+            users.append(line.split(":")[0]) # only take user's name from /etc/passwd
 
+# read authorized_users.txt and append to auth_users list
+if os.stat("./authorized_users.txt").st_size != 0: # authorized-users.txt isnt empty
+    with open('./authorized-users.txt', 'r') as authorized_users:
+        for user in authorized_users.readlines():
+            auth_users.append(user)
+
+# set good password aging policies for current users
 for user in users:
     os.system(f"sudo chage --mindays 7 --maxdays 90 --warndays 5 {user}")
 
 print("Set system password aging policies for current users.")
 
 # check if any users are unauthorized on the system and ask whether to delete or not
-with open('./authorized-users.txt', 'r') as authorized_users:
-    print(authorized_users)
+for user in users:
+    if user not in auth_users:
+        remove_user = input(f"Remove user {user}? They are not in authorized_users.txt. (WARNING: Double check before answering!) (y/n)").lower()
+        if remove_user == "y" or remove_user == "yes":
+            pass # remove user here
+        elif remove_user == "n" or remove_user == "no":
+            print("Not removing user.")
+        else:
+            print("Invalid input; defaulting to not removing user.")
