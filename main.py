86e66f1d9427cd336408b8f683ad6021927e5c6a
Storm <stormpower44@protonmail.com>
2023-11-15 19:12:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ print("Set system password aging policies for current users.")
 for auth_user in auth_users:
     if auth_user != "DISABLED":
         if auth_user not in auth_users:
-            remove_user = input(f"Remove user {user}? They are not in authorized_users.txt. (WARNING: Double check before answering!) (y/n)").lower()
+            remove_user = input(f"Remove user {auth_user}? They are not in authorized_users.txt. (WARNING: Double check before answering!) (y/n)").lower()
             if remove_user == "y" or remove_user == "yes":
                 pass # remove user here
             elif remove_user == "n" or remove_user == "no":
